17417818bf303026f3756fc0aa502ed50eb5a776
el634dev <al7758066@gmail.com>
2026-02-03 12:01:57 -0500

    Add padding to button and increased the font size

--- app/src/main/java/com/example/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.temperatureconverter.ui.theme.TemperatureConverterTheme
 
@@ -91,7 +92,8 @@ fun ConverterScreen(modifier: Modifier = Modifier) {
         ) {
             Text(
                 text = "Calculate",
-                fontSize = 20.sp
+                fontSize = 20.sp,
+                modifier = Modifier.padding(10.dp)
             )
         }
     }
